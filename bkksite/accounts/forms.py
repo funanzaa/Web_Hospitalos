df2d88c7a765045f905a7a23fca5c31b668fe64b
funanzaa <eakachai_golf@hotmail.com>
2020-09-01 16:55:10 +0700

    add form-group

--- bkksite/accounts/forms.py
+++ bkksite/accounts/forms.py
@@ -9,4 +9,8 @@ class UserCreateForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields["username"].label = "Display name"
+        self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields["email"].label = "Email address"
+        self.fields['email'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
